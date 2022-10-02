3e622a5601b18047e04abf390be8c3de62d6bfb4
obiito21 <luis.carrasquilla.z@gmail.com>
2022-10-02 13:26:58 -0500

    my first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './index.css';
+import './App.css';
+import TodoForm from "./components/form";
+import Todo from "./components/todo";
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  return(
+    <div className="app">
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/form.js
@@ -0,0 +1,28 @@
+// user input - includes validation
+import React from 'react';
+import { useState } from "react";
+
+
+const TodoForm = ({addTodo}) => {
+  const [value,setValue] = React.useState('');
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
+export default TodoForm;
